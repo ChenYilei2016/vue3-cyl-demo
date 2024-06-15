08aaf3a9a163824d0513773fd94e8f59bf9117b1
qingshi <705029004@qq.com>
2024-06-15 22:31:20 +0800

    ts

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,47 @@
-
-
-<template>
-  <h1 v-text="a + 1"></h1>
-  <h2>{{a + 2}}</h2>
-  <button @click="increment">Count is: {{ a }}</button>
-  <button @click="increment2">Count is: {{ a }}</button>
-  <button @click="change">Count is: {{ b }}</button>
-</template>
-
-<script setup>
-import {ref,onMounted} from "vue";
-  const a = ref(2)
-  const b = ref('hello world')
-
-  const change = ()=>{
-    b.value = b.value.split(" ").reverse().join(" ")
-  }
-
-  // 用来修改状态、触发更新的函数
-  function increment() {
-    a.value++
-  }
-  const increment2 = ()=>{
-    a.value++
-  }
-  // 生命周期钩子
-  onMounted(() => {
-    console.log(`The initial count is ${a.value}.`)
-  })
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style scoped>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="assets/logo.svg" width="125" height="125" />
 
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
---
+++ src/App111.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+  <h1 v-text="a + 1"></h1>
+  <h2>{{a + 2}}</h2>
+  <button @click="increment">Count is: {{ a }}</button>
+  <button @click="increment2">Count is: {{ a }}</button>
+  <button @click="change">Count is: {{ b }}</button>
+</template>
+
+<script setup>
+import {ref,onMounted} from "vue";
+  const a = ref(2)
+  const b = ref('hello world')
+
+  const change = ()=>{
+    b.value = b.value.split(" ").reverse().join(" ")
+  }
+
+  // 用来修改状态、触发更新的函数
+  function increment() {
+    a.value++
+  }
+  const increment2 = ()=>{
+    a.value++
+  }
+  // 生命周期钩子
+  onMounted(() => {
+    console.log(`The initial count is ${a.value}.`)
+  })
+</script>
+
+<style scoped>
+
+</style>
